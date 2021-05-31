7a928f37abd8d4be5e249d2d78a0aa311dee6d69
Akshive <pandeyakshive97@gmail.com>
2021-05-31 07:16:32 +0530

    adding browse microfrontends

---
+++ src/Loading.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import styled from "styled-components";
+
+const Loading = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+`;
+export default () => <Loading>Loading...</Loading>;
